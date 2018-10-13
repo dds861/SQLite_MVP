d58914e21e202963ad7b1376d502b73b05d3ace9
dds861 <dds861@gmail.com>
2018-10-13 23:20:52 +0100

    optimized code

--- app/src/main/java/com/dd/database/sqliteMVP/View/DatabaseOpenHelper.java
+++ app/src/main/java/com/dd/database/sqliteMVP/View/DatabaseOpenHelper.java
@@ -13,7 +13,6 @@ public class DatabaseOpenHelper extends SQLiteAssetHelper {
     private static final String DATABASE_NAME = "makal.db";
     private static final int DATABASE_VERSION = 1;
 
-
     public DatabaseOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
